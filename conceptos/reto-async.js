f12c829b53440d1f7c45b1f3d9907019bd99a847
Misael Calvillo Mancilla <misa@homely.mx>
2022-11-17 20:47:33 -0600

    Reto de let/var y hoisting

---
+++ conceptos/reto-async.js
@@ -0,0 +1,12 @@
+
+for(let i = 1; i <= 3; i++){
+  setTimeout(() =>{
+    console.log(i)
+  }, 1000 * i)
+}
+
+// for(var i = 0; i < 3; i++  ){
+//   setTimeout(() =>{
+//     console.log(i)
+//   },3000)
+// }
