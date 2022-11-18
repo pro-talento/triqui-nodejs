06c343252ccecd9b599d54447f5fd28440aece0c
Misael Calvillo Mancilla <misa@homely.mx>
2022-11-17 20:34:09 -0600

    Tarea

--- tareas/promesas.js
+++ tareas/promesas.js
@@ -1,1 +1,15 @@
 // Aquí va tu tarea
+
+// 1. Crea una rama con la sintaxis promesas-<usuario de github> sin los signos <>. 
+// 2. Subir la rama a GitHub
+// 3. En tu rama, en este archivo, realizar la siguiente actividad:
+//    a) Crear una cadena de procesos asíncronos 
+//    b) Ejecutarlos de forma ordenada 
+//    c) Mostrar más de una forma de ejecutarlos (Ejemplo: async/await  y  then/catch)
+
+//   Nota. El mínimo numero de procesos es 4. 
+
+//   Extra: Usar try/catch en algún proceso para manejar error
+//   Extra: Usar if/else para hacer un poco más complejo el proceso
+
+// 4. En cuanto este terminada la actividad. Hacer push de los cambios a tu rama en Github. 
