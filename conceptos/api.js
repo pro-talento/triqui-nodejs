4dbfaccf46bee4b0f35132e2c8716aa6eb70c62e
Misael Calvillo Mancilla <misa@homely.mx>
2022-11-22 19:08:25 -0600

    api - Agrega otras monedas

--- conceptos/api.js
+++ conceptos/api.js
@@ -2,5 +2,10 @@ const fetch = require('node-fetch');
 
 fetch('https://api.coindesk.com/v1/bpi/currentprice.json') // -> Promesa
   .then((res) => res.json()) // -> Promesa
-  .then((body) => console.log(`El Bitcoin cuesta ${body.bpi.USD.rate} dólares.`))
+  .then((body) => {
+    console.log(`El Bitcoin cuesta ${body.bpi.USD.rate} dólares.`)
+    console.log(`El Bitcoin cuesta ${body.bpi.GBP.rate} libras.`)
+    console.log(`El Bitcoin cuesta ${body.bpi.EUR.rate} euros.`)
+    // console.log(body);
+  })
   .catch((error) => console.error(error))
