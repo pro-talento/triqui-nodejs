9497869c8cdeca593cc92006d2ecf03b649897eb
Cristian Fabian Angel Ceballos <97754645+Angel1805@users.noreply.github.com>
2022-11-24 21:02:22 -0500

    ejercicio

--- tareas/api-spotify.js
+++ tareas/api-spotify.js
@@ -1,2 +1,15 @@
 // api-spotify-NombreDeUsuario
-console.log('API de Spotify')
+console.log('API de Spotify');
+
+const fetch = require('node-fetch');
+
+fetch("https://api.spotify.com/v1/search?q=bunny&type=track%2Cartist&market=ES&limit=10&offset=5", {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${"BQAI7qPBRdeG3EYam92Wez9mAPEUT2mSyU75tMr_hersnDcmpjCS-UcumtTtTEzKwpbFOLoMeHZAMhiMoQ8FO8y0AG7FYE7va19NMSPvyu6mQgh1bVu27gEGq_mDkYDA82Y-J7YeXk_cABknDMm3IEe9sI6KnGeIvoSxRa2gjq1eHkd4lzlKTKV7qoTwYP1ry8Q"}`
+  }
+})
+.then(response => response.json())
+.then((body) => {
+    console.log(body.tracks.items[0]);
+  })
